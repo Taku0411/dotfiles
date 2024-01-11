d8ad4768c32f6793bf02ab6b5b10a9c4a3c29169
Taku0411 <19991104taku@gmail.com>
2024-01-11 11:33:38 +0900

    added clang-tidy & textlint

---
+++ textlint/install.sh
@@ -0,0 +1,10 @@
+# install textlint
+npm install textlint
+npm install textlint-rule-preset-ja-spacing textlint-rule-preset-ja-technical-writing
+npm install textlint-plugin-latex2e
+
+# install efm-lang-server
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+go install github.com/mattn/efm-langserver@latest
+ln -s ${PWD}/config.yaml ~/.config/efm-langserver/config.yaml
